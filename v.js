c38f594b9c0dc387ec7c7af60f717623d108e5f5
Hans Felde <m33pfish@gmail.com>
2016-03-01 08:36:50 -0600

    Update v.js

--- v.js
+++ v.js
@@ -1,1 +1,37 @@
+var v = {}
 
+v.lex = function(code) {
+  var tokens = []
+  var token = ""
+  var inQuotes = false
+  var escaped = false
+  var next = false
+  var ws = false
+  var nl = false
+  for (var i = 0; i < code.length; i++) {
+    if (next) {
+      
+    }
+    if (!escaped) {
+      if (inQuotes) {
+        if (code[i] == "\"") {
+          inQuotes = false
+          next = true
+        }
+      } else {
+        if (/^(,|\.\.\.|<--|<-\||<-|=|:|\^|&|\||is(nt)?|<<|>>|-|\+|%|\/|\*|\*\*|\/\/|_|\.)/gm.test(code)) {
+          tokens.push(/^(,|\.\.\.|<--|<-\||<-|=|:|\^|&|\||is(nt)?|<<|>>|-|\+|%|\/|\*|\*\*|\/\/|_|\.)/gm.exec(code))
+          code = code.replace(/^(,|\.\.\.|<--|<-\||<-|=|:|\^|&|\||is(nt)?|<<|>>|-|\+|%|\/|\*|\*\*|\/\/|_|\.)/gm, "")
+          next = true
+        }
+      }
+    }
+    token += code[i]
+  }
+}
+v.parse = function(code) {
+  code = this.lex(code)
+}
+v.exec = function(code) {
+  code = this.parse(code)
+}
